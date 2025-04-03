6ccd1ec3e3a76656cd2c918dbd1f7ddc258f142c
Daniel <kotoriysamposebe@gmail.com>
2025-04-03 18:20:35 +0700

    Permission

---
+++ database/migrations/2025_04_03_103339_create_view_drivers_permission.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $permission = Permission::firstOrCreate(['name' => 'view_drivers']);
+
+        $roles = Role::whereIn('name', ['super-admin', 'admin', 'support'])->get();
+
+        foreach ($roles as $role) {
+            $role->givePermissionTo($permission);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Permission::where('name', 'view_drivers')->delete();
+    }
+};
